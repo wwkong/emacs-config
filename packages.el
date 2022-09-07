8cc3b6b16c30c37c9fef2b6b3736a4f00e79ffc9
William Kong <wwkong92@gmail.com>
2022-09-06 21:58:20 -0400

    Small simplifications

--- packages.el
+++ packages.el
@@ -13,9 +13,6 @@
 ;; Config packages
 ;; ----------------------------------------------------------------------------
 
-(package! dap-mode)
-(package! all-the-icons-dired)
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
